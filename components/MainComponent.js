6bed6e0f279305c028aaf78d31be20fec63e156f
MARJORIE CICERON <jorie@marjories-mbp.myfiosgateway.com>
2021-03-09 21:02:28 -0500

    Activity indicator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,7 +10,15 @@ import { createAppContainer } from 'react-navigation';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
 
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -197,6 +205,12 @@ const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
    
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
 
     render() {
         return (
@@ -242,4 +256,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
